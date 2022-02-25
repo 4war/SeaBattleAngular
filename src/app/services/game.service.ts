5aab9d719dfbf9d7900eebaad7e856d9a9bc0434
4war <fedro_777@mail.ru>
2022-02-25 13:07:29 +0400

    also added pngs

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GameService {
+
+
+
+  //todo: Принять cell
+  //todo: Передать в stateComponent и обновить его
+
+}
+
